2aaf4a76a1afcff60eec8f27ae46ac9b21e6ecbf
dkhorev <me@dkhorev.pro>
2023-01-11 12:42:03 -0500

    add response codes

--- users-api-native/index.js
+++ users-api-native/index.js
@@ -5,33 +5,29 @@ const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader("Content-Type", "application/json");
 
-  if (req.method === "POST") {
-    if (req.url === "/users/register") {
-      let body = "";
-      req.on("data", function (chunk) {
-        body += chunk;
-      });
+  if (req.method === "POST" && req.url === "/users/register") {
+    let body = "";
+    req.on("data", function (chunk) {
+      body += chunk;
+    });
 
-      req.on("end", function () {
-        const data = JSON.parse(body);
+    req.on("end", function () {
+      const data = JSON.parse(body);
 
-        try {
-          const newUser = usersApi.register(data.email, data.name);
-          res.end(JSON.stringify(newUser));
-        } catch (e) {
-          res.statusCode = 422;
-          res.end(JSON.stringify({ error: e.message }));
-        }
-      });
+      try {
+        const newUser = usersApi.register(data.email, data.name);
+        res.end(JSON.stringify(newUser));
+      } catch (e) {
+        res.statusCode = 422;
+        res.end(JSON.stringify({ error: e.message }));
+      }
+    });
 
-      return;
-    }
-  } else if (req.method === "GET") {
-    if (req.url === "/users") {
-      res.end(JSON.stringify(usersApi.all()));
+    return;
+  } else if (req.method === "GET" && req.url === "/users") {
+    res.end(JSON.stringify(usersApi.all()));
 
-      return;
-    }
+    return;
   }
 
   res.end("Route not defined");
